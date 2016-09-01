076eec0c9ae6a79c0a22fccc33bef74ce3653e30
William T <william.tam18@gmail.com>
2016-09-02 01:01:35 +0800

    boilerplating finished

---
+++ src/main.js
@@ -0,0 +1,11 @@
+console.log('I am here and running.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
